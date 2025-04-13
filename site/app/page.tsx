5e26205e42d37b61e0e4439db7d5e0d2293cd4d7
Nikhil <tobecoolaf@gmail.com>
2025-04-13 09:41:10 +0530

    structure: fixed project layout

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from './page.module.css';
-import HeroSection from './ui/HeroSection';
+import HeroSection from './components/HeroSection';
 
 export default function Home() {
 	return (
