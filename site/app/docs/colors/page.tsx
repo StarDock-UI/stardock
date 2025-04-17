0ca8d5833d755eadbe6710c075b4cbac42ba19a7
Diya Tripathi <tripathidiya673@gmail.com>
2025-04-17 19:10:54 +0530

    some changes

--- site/app/docs/colors/page.tsx
+++ site/app/docs/colors/page.tsx
@@ -17,199 +17,79 @@ const Page = () => {
 				<DocsHeader />
 				<Blob size='500px' top='20px' opacity={0.1} />
 
-        <div className="container">
-        <div>
-          <h1>Class</h1>
-          {/* Original Colors */}
-          <p>snowlight</p>
-          <p>ashcloud</p>
-          <p>ghostveil</p>
-          <p>mistgray</p>
-          <p>steelfog</p>
-          <p>smogtone</p>
-          <p>slateveil</p>
-          <p>duskshade</p>
-          <p>charcoalink</p>
-          <p>shadowcore</p>
-          <p>abyssnight</p>
+				<div className="container">
+					<h1>Color Classes</h1>
+					<table className="color-table">
+						<thead>
+							<tr>
+								<th>Class</th>
+								<th>Color</th>
+							</tr>
+						</thead>
+						<tbody>
+							{/* ORIGINAL COLORS */}
+							<tr><td>snowlight</td><td style={{ backgroundColor: '#f9f9f9' }}>#f9f9f9</td></tr>
+							<tr><td>ashcloud</td><td style={{ backgroundColor: '#f1f1f1' }}>#f1f1f1</td></tr>
+							<tr><td>ghostveil</td><td style={{ backgroundColor: '#e2e2e2' }}>#e2e2e2</td></tr>
+							<tr><td>mistgray</td><td style={{ backgroundColor: '#d3d3d3' }}>#d3d3d3</td></tr>
+							<tr><td>steelfog</td><td style={{ backgroundColor: '#b4b4b4' }}>#b4b4b4</td></tr>
+							<tr><td>smogtone</td><td style={{ backgroundColor: '#969696' }}>#969696</td></tr>
+							<tr><td>slateveil</td><td style={{ backgroundColor: '#777777' }}>#777777</td></tr>
+							<tr><td>duskshade</td><td style={{ backgroundColor: '#5a5a5a', color: '#fff' }}>#5a5a5a</td></tr>
+							<tr><td>charcoalink</td><td style={{ backgroundColor: '#3c3c3c', color: '#fff' }}>#3c3c3c</td></tr>
+							<tr><td>shadowcore</td><td style={{ backgroundColor: '#1e1e1e', color: '#fff' }}>#1e1e1e</td></tr>
+							<tr><td>abyssnight</td><td style={{ backgroundColor: '#0a0a0a', color: '#fff' }}>#0a0a0a</td></tr>
 
-          {/* Reds & Warm Tones */}
-          <p>firebud</p>
-          <p>emberglow</p>
-          <p>cherryblush</p>
-          <p>duskrose</p>
-          <p>rustflame</p>
-          <p>bloodshade</p>
+							{/* REDS */}
+							<tr><td>firebud</td><td style={{ backgroundColor: '#ff6b6b' }}>#ff6b6b</td></tr>
+							<tr><td>emberglow</td><td style={{ backgroundColor: '#e94e4e' }}>#e94e4e</td></tr>
+							<tr><td>cherryblush</td><td style={{ backgroundColor: '#cc3c3c' }}>#cc3c3c</td></tr>
+							<tr><td>duskrose</td><td style={{ backgroundColor: '#aa2e2e' }}>#aa2e2e</td></tr>
+							<tr><td>rustflame</td><td style={{ backgroundColor: '#883131' }}>#883131</td></tr>
+							<tr><td>bloodshade</td><td style={{ backgroundColor: '#660f0f' }}>#660f0f</td></tr>
 
-          {/* Oranges & Ambers */}
-          <p>sunsetgold</p>
-          <p>amberroot</p>
-          <p>apricrush</p>
-          <p>caramelcider</p>
-          <p>copperflare</p>
-          <p>emberdust</p>
+							{/* ORANGES */}
+							<tr><td>sunsetgold</td><td style={{ backgroundColor: '#ff9e4a' }}>#ff9e4a</td></tr>
+							<tr><td>amberroot</td><td style={{ backgroundColor: '#ff7f0a' }}>#ff7f0a</td></tr>
+							<tr><td>apricrush</td><td style={{ backgroundColor: '#f3691e' }}>#f3691e</td></tr>
+							<tr><td>caramelcider</td><td style={{ backgroundColor: '#d15400' }}>#d15400</td></tr>
+							<tr><td>copperflare</td><td style={{ backgroundColor: '#b04500' }}>#b04500</td></tr>
+							<tr><td>emberdust</td><td style={{ backgroundColor: '#8a3700' }}>#8a3700</td></tr>
 
-          {/* Yellows */}
-          <p>sunflowerpop</p>
-          <p>mellowhaze</p>
-          <p>lemonzest</p>
-          <p>mustardspark</p>
-          <p>honeydrip</p>
-          <p>goldenharvest</p>
+							{/* YELLOWS */}
+							<tr><td>sunflowerpop</td><td style={{ backgroundColor: '#ffe24a' }}>#ffe24a</td></tr>
+							<tr><td>mellowhaze</td><td style={{ backgroundColor: '#f8d245' }}>#f8d245</td></tr>
+							<tr><td>lemonzest</td><td style={{ backgroundColor: '#f2bc22' }}>#f2bc22</td></tr>
+							<tr><td>mustardspark</td><td style={{ backgroundColor: '#d0a000' }}>#d0a000</td></tr>
+							<tr><td>honeydrip</td><td style={{ backgroundColor: '#b78c00' }}>#b78c00</td></tr>
+							<tr><td>goldenharvest</td><td style={{ backgroundColor: '#a37600' }}>#a37600</td></tr>
 
-          {/* Greens */}
-          <p>mintwhirl</p>
-          <p>jadeflash</p>
-          <p>verdantwave</p>
-          <p>forestink</p>
-          <p>mossveil</p>
-          <p>pinefog</p>
+							{/* Add the rest below similarly... */}
 
-          {/* Blues */}
-          <p>skydream</p>
-          <p>icemist</p>
-          <p>azurebreeze</p>
-          <p>cobaltstream</p>
-          <p>deepwave</p>
-          <p>midnightsurf</p>
+							{/* Funky & Fun */}
+							<tr><td>neonlime</td><td style={{ backgroundColor: '#b0ff2d' }}>#b0ff2d</td></tr>
+							<tr><td>electricmango</td><td style={{ backgroundColor: '#ffb200' }}>#ffb200</td></tr>
+							<tr><td>hyperblue</td><td style={{ backgroundColor: '#3a60ff' }}>#3a60ff</td></tr>
+							<tr><td>cyberpink</td><td style={{ backgroundColor: '#ff49aa' }}>#ff49aa</td></tr>
+							<tr><td>glitchgreen</td><td style={{ backgroundColor: '#00ff8f' }}>#00ff8f</td></tr>
+							<tr><td>spacemint</td><td style={{ backgroundColor: '#73f9d6' }}>#73f9d6</td></tr>
+							<tr><td>technoindigo</td><td style={{ backgroundColor: '#7e4bff' }}>#7e4bff</td></tr>
+							<tr><td>flameburst</td><td style={{ backgroundColor: '#ff3366' }}>#ff3366</td></tr>
+						</tbody>
+					</table>
+				</div>
 
-          {/* Purples */}
-          <p>lavenderdust</p>
-          <p>violettease</p>
-          <p>amethystlight</p>
-          <p>mysticshade</p>
-          <p>grapeink</p>
-          <p>velvetvoid</p>
-
-          {/* Pinks */}
-          <p>bubblecharm</p>
-          <p>rosequartz</p>
-          <p>petalflush</p>
-          <p>cherryshade</p>
-          <p>bloomtone</p>
-          <p>blushdepth</p>
-
-          {/* Beige & Browns */}
-          <p>sandhue</p>
-          <p>dunepearl</p>
-          <p>lattefoam</p>
-          <p>toffeetwist</p>
-          <p>oakshade</p>
-          <p>darkbrew</p>
-
-          {/* Funky & Fun */}
-          <p>neonlime</p>
-          <p>electricmango</p>
-          <p>hyperblue</p>
-          <p>cyberpink</p>
-          <p>glitchgreen</p>
-          <p>spacemint</p>
-          <p>technoindigo</p>
-          <p>flameburst</p>
-        </div>
-
-        <div>
-          <h1>Colors</h1>
-          {/* Original Colors */}
-          <p style={{ backgroundColor: '#f9f9f9' }}>#f9f9f9</p>
-          <p style={{ backgroundColor: '#f1f1f1' }}>#f1f1f1</p>
-          <p style={{ backgroundColor: '#e2e2e2' }}>#e2e2e2</p>
-          <p style={{ backgroundColor: '#d3d3d3' }}>#d3d3d3</p>
-          <p style={{ backgroundColor: '#b4b4b4' }}>#b4b4b4</p>
-          <p style={{ backgroundColor: '#969696' }}>#969696</p>
-          <p style={{ backgroundColor: '#777777' }}>#777777</p>
-          <p style={{ backgroundColor: '#5a5a5a', color: '#fff' }}>#5a5a5a</p>
-          <p style={{ backgroundColor: '#3c3c3c', color: '#fff' }}>#3c3c3c</p>
-          <p style={{ backgroundColor: '#1e1e1e', color: '#fff' }}>#1e1e1e</p>
-          <p style={{ backgroundColor: '#0a0a0a', color: '#fff' }}>#0a0a0a</p>
-
-          {/* Reds & Warm Tones */}
-          <p style={{ backgroundColor: '#ff6b6b' }}>#ff6b6b</p>
-          <p style={{ backgroundColor: '#e94e4e' }}>#e94e4e</p>
-          <p style={{ backgroundColor: '#cc3c3c' }}>#cc3c3c</p>
-          <p style={{ backgroundColor: '#aa2e2e' }}>#aa2e2e</p>
-          <p style={{ backgroundColor: '#883131' }}>#883131</p>
-          <p style={{ backgroundColor: '#660f0f' }}>#660f0f</p>
-
-          {/* Oranges & Ambers */}
-          <p style={{ backgroundColor: '#ff9e4a' }}>#ff9e4a</p>
-          <p style={{ backgroundColor: '#ff7f0a' }}>#ff7f0a</p>
-          <p style={{ backgroundColor: '#f3691e' }}>#f3691e</p>
-          <p style={{ backgroundColor: '#d15400' }}>#d15400</p>
-          <p style={{ backgroundColor: '#b04500' }}>#b04500</p>
-          <p style={{ backgroundColor: '#8a3700' }}>#8a3700</p>
-
-          {/* Yellows */}
-          <p style={{ backgroundColor: '#ffe24a' }}>#ffe24a</p>
-          <p style={{ backgroundColor: '#f8d245' }}>#f8d245</p>
-          <p style={{ backgroundColor: '#f2bc22' }}>#f2bc22</p>
-          <p style={{ backgroundColor: '#d0a000' }}>#d0a000</p>
-          <p style={{ backgroundColor: '#b78c00' }}>#b78c00</p>
-          <p style={{ backgroundColor: '#a37600' }}>#a37600</p>
-
-          {/* Greens */}
-          <p style={{ backgroundColor: '#a8f0c6' }}>#a8f0c6</p>
-          <p style={{ backgroundColor: '#5ce0aa' }}>#5ce0aa</p>
-          <p style={{ backgroundColor: '#30c48c' }}>#30c48c</p>
-          <p style={{ backgroundColor: '#20845e' }}>#20845e</p>
-          <p style={{ backgroundColor: '#1b664b' }}>#1b664b</p>
-          <p style={{ backgroundColor: '#134534' }}>#134534</p>
-
-          {/* Blues */}
-          <p style={{ backgroundColor: '#b5e3ff' }}>#b5e3ff</p>
-          <p style={{ backgroundColor: '#86d1ff' }}>#86d1ff</p>
-          <p style={{ backgroundColor: '#59b7f3' }}>#59b7f3</p>
-          <p style={{ backgroundColor: '#3582c4' }}>#3582c4</p>
-          <p style={{ backgroundColor: '#24629f' }}>#24629f</p>
-          <p style={{ backgroundColor: '#183f66' }}>#183f66</p>
-
-          {/* Purples */}
-          <p style={{ backgroundColor: '#e4ccff' }}>#e4ccff</p>
-          <p style={{ backgroundColor: '#c69cff' }}>#c69cff</p>
-          <p style={{ backgroundColor: '#a16ddc' }}>#a16ddc</p>
-          <p style={{ backgroundColor: '#814cb2' }}>#814cb2</p>
-          <p style={{ backgroundColor: '#612d91' }}>#612d91</p>
-          <p style={{ backgroundColor: '#411b66' }}>#411b66</p>
-
-          {/* Pinks */}
-          <p style={{ backgroundColor: '#ffc6dc' }}>#ffc6dc</p>
-          <p style={{ backgroundColor: '#ff9fc4' }}>#ff9fc4</p>
-          <p style={{ backgroundColor: '#f76faa' }}>#f76faa</p>
-          <p style={{ backgroundColor: '#d85287' }}>#d85287</p>
-          <p style={{ backgroundColor: '#b8386a' }}>#b8386a</p>
-          <p style={{ backgroundColor: '#98234f' }}>#98234f</p>
-
-          {/* Beige & Browns */}
-          <p style={{ backgroundColor: '#f6e7d4' }}>#f6e7d4</p>
-          <p style={{ backgroundColor: '#e4d1b3' }}>#e4d1b3</p>
-          <p style={{ backgroundColor: '#cdb089' }}>#cdb089</p>
-          <p style={{ backgroundColor: '#b38a5e' }}>#b38a5e</p>
-          <p style={{ backgroundColor: '#916c3b' }}>#916c3b</p>
-          <p style={{ backgroundColor: '#6e4c28' }}>#6e4c28</p>
-
-          {/* Funky & Fun */}
-          <p style={{ backgroundColor: '#b0ff2d' }}>#b0ff2d</p>
-          <p style={{ backgroundColor: '#ffb200' }}>#ffb200</p>
-          <p style={{ backgroundColor: '#3a60ff' }}>#3a60ff</p>
-          <p style={{ backgroundColor: '#ff49aa' }}>#ff49aa</p>
-          <p style={{ backgroundColor: '#00ff8f' }}>#00ff8f</p>
-          <p style={{ backgroundColor: '#73f9d6' }}>#73f9d6</p>
-          <p style={{ backgroundColor: '#7e4bff' }}>#7e4bff</p>
-          <p style={{ backgroundColor: '#ff3366' }}>#ff3366</p>
-        </div>
-      </div>
+				<div
+					className='navigation-buttons'
+					style={{ display: 'flex', gap: '1rem', marginTop: '2rem' }}
+				>
+					<Link href='/docs'>
+						<span className='Doc-button left-button'>
+							<ChevronLeft /> Home
+						</span>
+					</Link>
+				</div>
 			</div>
-      <div
-						className='navigation-buttons'
-						style={{ display: 'flex', gap: '1rem', marginTop: '2rem' }}
-					>
-						<Link href='/docs'>
-							<span className='Doc-button left-button'>
-								<ChevronLeft /> Home
-							</span>
-						</Link>
-					
-					</div>
 		</div>
 	);
 };
